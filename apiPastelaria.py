b2ac20f3a1ecb113a69a935eec0bc77ade27bed8
thiago <thiag@NOTETHIAGO>
2023-09-05 16:29:47 -0300

    testes

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -5,14 +5,14 @@ from settings import HOST, PORT, RELOAD
 # import das classes com as rotas/endpoints
 from mod_funcionario import FuncionarioDAO
 from mod_cliente import ClienteDAO 
-from mod_produto import ProdutoDAO
+from mod_produto import ProdutoDao
 
 app = FastAPI()
 
 # mapeamento das rotas/endpoints
 app.include_router(FuncionarioDAO.router)
 app.include_router(ClienteDAO.router)
-app.include_router(ProdutoDAO.router)
+app.include_router(ProdutoDao.router)
 
 # rota padrão
 @app.get("/")
